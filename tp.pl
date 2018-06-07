275eec895f7006d08b3ebd59cc7b15c4579f1df7
eliascosta <eliazcost@hotmail.es>
2018-06-07 01:07:24 -0300

    Add files via upload

---
+++ tp.pl
@@ -0,0 +1,18 @@
+%base_de_conocimientos
+
+popular(got).
+popular(hoc).
+popular(starWars).
+mira(juan, himym).
+mira(juan, futurama).
+mira(juan, got).
+mira(nico, starWars).
+mira(nico,got).
+mira(maiu, starWars).
+mira(maiu, onePiece).
+mira(maiu, got).
+mira(gaston, got).
+quiere_ver(juan, hoc).
+quiere_ver(aye, got).
+quiere_ver(gaston,himym).
+
