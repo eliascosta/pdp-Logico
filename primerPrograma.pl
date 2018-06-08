795df8d2278ed70ddd9d389bef6c9d6c797ab7a1
Elias Costa <eliazcost@hotmail.es>
2018-06-07 22:51:46 -0300

    Update primerPrograma.pl

--- primerPrograma.pl
+++ primerPrograma.pl
@@ -1,8 +1,17 @@
+%Base_conocimiento.
+%hechos:
 morocha(maria).
 tieneOnda(ana).
 morocha(ana).
 simpatica(cristina).
 gusta(juan,cristina).
 gusta(juan,maria).
+gusta(pedro, ana).
+gusta(pedro, nora).
+gusta(pedro, luisa).
+%reglas:
+gusta(Persona, zulema):- gusta(Persona, nora).
+gusta(julian,Persona):- morocha(Persona),tieneOnda(Persona).
+gusta(mario, Persona):- morocha(Persona),tieneOnda(Persona).
+gusta(Persona, laura):- gusta(Persona,ana), gusta(Persona,luisa).
 gusta(P1,P2):- simpatica(P2),gusta(P1,ana).
-	
